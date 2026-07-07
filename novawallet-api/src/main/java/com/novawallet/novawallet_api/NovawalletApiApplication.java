a1d842b7f3c467a0249bb51bd1e274b50359890f
Ivwananji Silungwe <ivwananji2004@gmail.com>
2026-07-07 18:06:17 +0200

    Create base package structure

---
+++ novawallet-api/src/main/java/com/novawallet/novawallet_api/NovawalletApiApplication.java
@@ -0,0 +1,13 @@
+package com.novawallet.novawallet_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovawalletApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovawalletApiApplication.class, args);
+	}
+
+}
---
+++ novawallet-api/src/test/java/com/novawallet/novawallet_api/NovawalletApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novawallet.novawallet_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovawalletApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
